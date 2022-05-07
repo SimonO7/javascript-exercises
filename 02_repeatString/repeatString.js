96f9cabf05cadbdf9569704986d746e6c232750d
SimonO7 <sdragonz461@gmail.com>
2022-05-06 23:09:23 -0700

    Pass test 1 repeating the string

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,11 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    let str = "";    //The final string
+    let count = 0;
+    while (count < num) {
+        str += string;
+        count++;
+    }
+    return str;
 };
 
 // Do not edit below this line
