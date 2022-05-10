8428fb5431fdf30020d9631c12b9dfbee98d708f
SimonO7 <sdragonz461@gmail.com>
2022-05-09 22:34:03 -0700

    Pass test 1

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,10 @@
-const sumAll = function() {
-
+const sumAll = function(start, end) {
+    let result = 0;
+    for (let num = start; num <= end; num++) {
+        result += num;
+    }
+    return result;
 };
-
+console.log(sumAll(0, 10));
 // Do not edit below this line
 module.exports = sumAll;
