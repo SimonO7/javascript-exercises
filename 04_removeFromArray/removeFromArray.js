c3039d6efd8ccebb4b9a4248c2c8f7299129000f
SimonO7 <sdragonz461@gmail.com>
2022-05-09 20:14:54 -0700

    Add documentation

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,8 +1,16 @@
 const removeFromArray = function(srcArray, elementsToRemove) {
+    //Take all elements after first element and store into array. These are elements to remove.
     elementsArrayToRemove = [...arguments].slice(1);
+
+    //Check each element to remove and remove it from array
     for (let element of elementsArrayToRemove) {
+        //Find the index of the element to remove in the source array
         const index = srcArray.indexOf(element);
+
+        //If the element is not there, index will be -1. If index is -1, move to next element.
         if (index === -1) continue;
+
+        //If index isn't -1, remove the element from the source array.
         srcArray.splice(index, 1);
     }
     return srcArray;
