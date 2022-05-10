6b7e78109cd207913a630fae2e1c11511e8580a3
SimonO7 <sdragonz461@gmail.com>
2022-05-09 23:13:01 -0700

    Include check for non numbers and check start num

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,10 +1,26 @@
-const sumAll = function(start, end) {
+const sumAll = function(num1, num2) {
     let result = 0;
+    let start, end;
+
+    if (num1 < 0 || num2 < 0 || typeof(num1) != "number" || typeof(num2) != "number") {
+        return "ERROR";
+    }
+
+    if (num1 < num2) {
+        start = num1;
+        end = num2;
+    }
+    else {
+        start = num2;
+        end = num1;
+    }
+
     for (let num = start; num <= end; num++) {
         result += num;
     }
+
     return result;
 };
-console.log(sumAll(0, 10));
+sumAll(1, 4)
 // Do not edit below this line
 module.exports = sumAll;
