988cbc3703b30b640e564a981ef1e2594ccfc621
SimonO7 <sdragonz461@gmail.com>
2022-05-09 23:16:35 -0700

    use isInteger method to check if input is integer

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -2,7 +2,7 @@ const sumAll = function(num1, num2) {
     let result = 0;
     let start, end;
 
-    if (num1 < 0 || num2 < 0 || typeof(num1) != "number" || typeof(num2) != "number") {
+    if (num1 < 0 || num2 < 0 || !Number.isInteger(num1) || !Number.isInteger(num2)) {
         return "ERROR";
     }
 
