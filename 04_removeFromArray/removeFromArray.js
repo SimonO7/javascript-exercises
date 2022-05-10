193cbfc8e35b091ea818c3abb2b134ee88489d2a
SimonO7 <sdragonz461@gmail.com>
2022-05-09 20:02:46 -0700

    Complete and pass test 1

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,10 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(srcArray, elementsToRemove) {
+    elementsArrayToRemove = [...arguments].slice(1);
+    for (let element of elementsArrayToRemove) {
+        const index = srcArray.indexOf(element);
+        srcArray.splice(index, 1);
+    }
+    return srcArray;
 };
 
 // Do not edit below this line
