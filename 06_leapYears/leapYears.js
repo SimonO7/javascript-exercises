ead654b34a78319daa1a2a75d3aab891e745d576
SimonO7 <sdragonz461@gmail.com>
2022-05-10 18:25:41 -0700

    Check for century and non century years

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,15 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+   //Test century years
+   if ((year % 100 === 0) && (year % 400 === 0)) {
+      return true;
+   }
+   //Test non century years
+   else if ((year % 100 !== 0) && (year % 4 === 0)) {
+      return true;
+   }
+   else {
+      return false;
+   }
 };
 
 // Do not edit below this line
