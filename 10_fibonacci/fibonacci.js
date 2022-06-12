db15ba709c15409eb7b8c066bf9e071d51b7a129
SimonO7 <sdragonz461@gmail.com>
2022-06-11 23:25:56 -0700

    Complete exercise using recuirsive method

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,17 @@
-const fibonacci = function() {
-
+const fibonacci = function(n) {
+    if (n < 0) {
+        return "OOPS"
+    }
+    if (n == 1) {
+        return 1;
+    }
+    if (n == 2) {
+        return 1;
+    }
+    return fibonacci(n-1) + fibonacci(n-2);
 };
+
+// console.log(fibonacci(15));
 
 // Do not edit below this line
 module.exports = fibonacci;
