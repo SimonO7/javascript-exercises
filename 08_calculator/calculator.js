85411715920d35866b649ad57080baa624355a14
SimonO7 <sdragonz461@gmail.com>
2022-06-11 17:41:59 -0700

    Update functions according to readme

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,37 @@
-const add = function() {
-	
+const add = function(operandOne, operandTwo) {
+	return operandOne + operandTwo;
 };
 
-const subtract = function() {
-	
+const subtract = function(operandOne, operandTwo) {
+	return operandOne - operandTwo;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  let total = 0;
+	for (let i = 0; i < arr.length; i++) {
+    total += arr[i];
+  }
+  return total;
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  let total = 1;
+  for (let i = 0; i < arr.length; i++) {
+    total *= arr[i];
+  }
+  return total;
 };
 
-const power = function() {
-	
+const power = function(base, exp) {
+	return base ** exp;
 };
 
-const factorial = function() {
-	
+const factorial = function(base) {
+	let total = 1;
+  for (let i = base; i > 0; i--) {
+    total *= i
+  }
+  return total;
 };
 
 // Do not edit below this line
