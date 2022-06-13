110cc3a32a385e0ea1cfe07ed8fd9a30bd27970e
SimonO7 <sdragonz461@gmail.com>
2022-06-12 23:19:06 -0700

    Complete assignment according to spec

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,4 +1,26 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    let oldestPerson;
+    let oldestAge;
+    for (let i = 0; i < people.length; i++) {
+        //Get the age
+        let age;
+        if (people[i]["yearOfDeath"]) {
+            age = people[i]["yearOfDeath"] - people[i]["yearOfBirth"];
+        }
+        else {
+            const currentYear = new Date().getFullYear();
+            age = currentYear - people[i]["yearOfBirth"];
+        }
+
+        //Check if it's the oldest age
+        if (!(oldestAge) || oldestAge < age) {
+            oldestPerson = people[i];
+            oldestAge = age;
+        }
+    }
+
+    //Return the oldest person's name
+    return oldestPerson;
 
 };
 
